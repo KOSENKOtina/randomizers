cc9b8488e28ce3c09b360b3efeb19701ada5db75
KOSENKOtina <31986559+KOSENKOtina@users.noreply.github.com>
2017-10-18 00:56:14 +0300

    Update rands.h

--- rands.h
+++ rands.h
@@ -5,16 +5,16 @@
 
 using namespace std;
 
-void plot_gisto(double(*f)());  //гістограма для 1-5 методів
-void plot_gisto3(double(*f)());  // гістограма для 6-8 методів
-void plot_gisto100(double(*f)()); // гістограма для 9, 10 методів
-double rand_1();  //Лінійний конгруентний метод
-double rand_2();  //Квадратичний конгруентний метод
-double rand_3();  //Числа Фібоначчі
-double rand_4();  //Обернена конгруентна послідовність
-double rand_5();  //Метод об’єднання
-double rand_6();  //Правило “3 сігма”
-double rand_7();  //Метод полярних координат
-double rand_8();  //Метод співвідношень
-double rand_9();  //Метод логарифму для генерування показового розподілу
-double rand_10(); //Метод Аренса для генерування гамма-розподілу порядку a > 1
+void plot_gisto(double(*f)());  
+void plot_gisto3(double(*f)()); 
+void plot_gisto100(double(*f)());
+double rand_1();  
+double rand_2();  
+double rand_3(); 
+double rand_4();
+double rand_5();
+double rand_6();
+double rand_7();
+double rand_8();
+double rand_9();
+double rand_10();
